124573537399cb4ed3a48b6645b162f3c402607c
leo <leowaldocontrabajo@gmail.com>
2021-03-11 11:33:47 -0300

    cambios nuevos

--- callbacks.js
+++ callbacks.js
@@ -1,2 +1,21 @@
-setTimeout()
-console.log('callbacks');
+setTimeout(()=>{/*un callback es una funcion q se pasa como parametro a otra funcion */
+  console.log('Hola Mundo');
+
+},2000)/*va a ejecutar una funcion callback despues de un determinado tiempo, el primer parametro es la funcion callback y el segundo es el tiempo */ 
+const saludar=()=>{
+  console.log('Hola Mundo nuevo');
+}
+setTimeout(saludar,2000);
+
+const getUsuarioByid=(id,callback)=>{/*callback el nombre de la funcion puede ser cualquier otro nombre */
+  const user={ /*defino un objeto usuario*/
+      id, /*ID del objeto usuario */
+      nombre:'leowaldo'/*nombre del usuario */ 
+  }
+    setTimeout(()=>{
+      callback(user);
+    },1000);
+}
+getUsuarioByid(10,({nombre,id})=>{/*simulamos el ID 10 y nos debe indicar el nombre de ese usuario*/
+  console.log(`nombre: ${nombre} Id: ${id}`);
+});
--- destructuracion.js
+++ destructuracion.js
@@ -1,24 +1,26 @@
-/*const personaje ={
-  nombre: 'pepe',
+/*const personaje ={ creo el objeto personaje
+  nombre: 'pepe',  atributos del objeto personaje
   apellido: 'Argento',
   profesion: 'zapatero',
-  getDescripcion(){
-    return `${nombre}${apellido} es${profesion}`
+  getDescripcion(){  metodo para obtener los atributos del personaje
+    return `${nombre}${apellido} es${profesion}` devuelve los atritubos
   }
 
 }
 /*const nombre=personaje.nombre;
 const apellido=personaje.apellido; con la destructuracion me ahorro estas lineas
-const profesion=personaje.profesion; */
+const profesion=personaje.profesion; 
+console.log(nombre,apellido,profesion); imprimo los atributos del objeto personaje
 
-/*const {nombre,apellido,profesion}=personaje;/*destructuracion */
+/*const {nombre,apellido,profesion, edad=50}=personaje;/*destructuracion agrego un atributo edad, y le agrego valor
+console.log(nombre,apellido,profesion,edad);
 
-/*const imprimrpersonaje=({nombre,apellido,profesion})=>{
+/*const imprimrpersonaje=({nombre,apellido,profesion})=>{ funcion imprimir personaje
  console.log(nombre,apellido,profesion);
 }
 
-imprimrpersonaje(personaje);*/
-const personajes =['harry','germione','ron'];
+imprimrpersonaje(personaje); llamo a la funcion imprimir personaje*/
+const personajes =['harry','germione','ron']; /*array personajes destructurar un array */
 /*const p1=personajes[0];
 const p2=personajes[1];
 const p3=personajes[2];
